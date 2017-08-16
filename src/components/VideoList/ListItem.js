a7c3dbb79913e5752b08af061eee26f03c02eef9
waheng <peterchang@yahoo-inc.com>
2017-08-16 09:31:19 +0800

    fbPageId -> fbId

--- src/components/VideoList/ListItem.js
+++ src/components/VideoList/ListItem.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 import classNames from 'classnames'
 import style from './ListItem.css'
 
-import VideoForm from'./VideoForm'
+import VideoForm from './VideoForm'
 
 class ListItem extends React.Component {
   // constructor (props) {
@@ -13,36 +13,36 @@ class ListItem extends React.Component {
 
   render() {
     const {s3Source, description, originThumb, fbPageName, fbPageId, title} = this.props
-    const pageLink = `https://facebook.com/${fbPageId}`
+    const pageLink = `https://facebook.com/${fbId}`
 
     return (
       <div className={classNames(style['row'])}>
         <h1 className={classNames(style['content-subhead'])}> {description} </h1>
         <section>
           <div className="pure-g">
-              <div className="pure-u-1-2">
-                  <header> 
-                    <h2 className={classNames(style['h2Description'])}> 
-                      {title} 
-                    </h2>
+            <div className="pure-u-1-2">
+              <header>
+                <h2 className={classNames(style['h2Description'])}>
+                  {title}
+                </h2>
 
-                    <a className={classNames(style['pageButton'] ,"pure-button" )}
-                      href = {pageLink}>
-                      {fbPageName}
-                    </a>
- 
+                <a className={classNames(style['pageButton'], 'pure-button')}
+                  href = {pageLink}>
+                  {fbPageName}
+                </a>
 
-                  </header>
 
-                  <div>
-                    <video className={classNames(style['videoStyle'])} controls poster={originThumb} preload="none" autoPlay="" loop="" muted="" data-reactid=".0.1.0.0">
-                      <source src={s3Source} type="video/mp4" />
-                    </video>
-                  </div>
+              </header>
+
+              <div>
+                <video className={classNames(style['videoStyle'])} controls poster={originThumb} preload="none" autoPlay="" loop="" muted="" data-reactid=".0.1.0.0">
+                  <source src={s3Source} type="video/mp4" />
+                </video>
               </div>
-              <div className="pure-u-1-2">
-                  <VideoForm />
-              </div>
+            </div>
+            <div className="pure-u-1-2">
+              <VideoForm />
+            </div>
           </div>
         </section>
       </div>
